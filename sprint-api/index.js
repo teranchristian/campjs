52ac3ec4b615ae4652cdcca779bf4e1a36deacd8
luca <luca.confa80@gmail.com>
2019-08-30 10:29:19 +1000

    add express app

---
+++ sprint-api/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(3003, () =>
+  console.log('Example app listening on port 3000!'),
+);
